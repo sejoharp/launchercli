8d22f05277635577c824263b0654859216d3fcfc
Frederik Mars <frederik.mars@gipedo.io>
2022-07-09 20:05:43 +0200

    move Item to separate file + rename to launchCommand

--- config_test.go
+++ config_test.go
@@ -9,7 +9,7 @@ func TestReadAndParseConfigFile(t *testing.T) {
 	config := readAndParseConfigFile("test-resources/valid_config.json")
 	expected := []ConfigItem{{
 		List:    "for directory in $(ls -d /home/user/repos/*); do echo $(basename $directory),$directory;done",
-		Command: "",
+		Command: "code",
 	}}
 	if !reflect.DeepEqual(config, expected) {
 		t.Errorf("Configs are not equal. Got %v, wanted %v", config, expected)
